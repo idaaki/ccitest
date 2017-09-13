41853ae02374db6737c41097d74c95a5cd6dc5b6
kamo <idaaki@gmail.com>
2017-09-13 15:13:40 +0900

    [update] seleniumテスト

--- src/tests/seleniumTest.php
+++ src/tests/seleniumTest.php
@@ -4,13 +4,13 @@ class seleniumTest extends PHPUnit_Extensions_Selenium2TestCase {
     protected function setUp()
     {
         $this->setHost("127.0.0.1");
-        $this->setPort(8080);
+        $this->setPort(4444);
         $this->setBrowserUrl('http://127.0.0.1/');
     }
 
     public function testA()
     {
-        $this->url("/");
+        $this->url("/src/");
 
         $this->byName('name')->value('name test');
         $this->byName('send')->click();
